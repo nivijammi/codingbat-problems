c7f1865a70fbe3f8bbb59546f09853574aaf6662
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-27 23:37:53 -0400

    Initial test commit

--- src/test/java/edu/codingbat/warmup1/Makes10Test.java
+++ src/test/java/edu/codingbat/warmup1/Makes10Test.java
@@ -1,4 +1,23 @@
+package edu.codingbat.warmup1;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class Makes10Test {
-  
+    @Test
+    public void shouldReturn10IfSum10OrOneIs10() {
+        Makes10 object = new Makes10();
+        assertTrue("one parameter is 10", object.makes10(9,10));
+    }
+    @Test
+    public void shouldReturn10IfSum10OrOneIs101() {
+        Makes10 object = new Makes10();
+        assertFalse("one parameter is 10", object.makes10(8,3));
+    }
+    @Test
+    public void shouldReturn10IfSum10OrOneIs102() {
+        Makes10 object = new Makes10();
+        assertTrue("one parameter is 10", object.makes10(12,-2));
+    }
 }
