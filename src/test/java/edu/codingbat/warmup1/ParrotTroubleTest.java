3595cfbbb7650d6c83386180585b0424259e834a
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-27 23:29:37 -0400

    Test cases for Parrot trouble

--- src/test/java/edu/codingbat/warmup1/ParrotTroubleTest.java
+++ src/test/java/edu/codingbat/warmup1/ParrotTroubleTest.java
@@ -6,6 +6,18 @@ import static org.junit.Assert.*;
 
 public class ParrotTroubleTest {
     @Test
-    public void name() {
+    public void shouldReturnTrueIfInTrouble() {
+        ParrotTrouble pt = new ParrotTrouble();
+        assertTrue(pt.parrotTrouble( true,6));
+    }
+    @Test
+    public void shouldReturnTrueIfInTrouble1() {
+        ParrotTrouble pt = new ParrotTrouble();
+        assertFalse(pt.parrotTrouble( true,7));
+    }
+    @Test
+    public void shouldReturnTrueIfInTrouble2() {
+        ParrotTrouble pt = new ParrotTrouble();
+        assertFalse(pt.parrotTrouble( false,6));
     }
 }
