e6e96f3f3ddadaea65bc1d8967a4884c566046d3
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-27 23:28:36 -0400

    parrot trouble

--- src/main/java/edu/codingbat/warmup1/ParrotTrouble.java
+++ src/main/java/edu/codingbat/warmup1/ParrotTrouble.java
@@ -1,6 +1,9 @@
 package edu.codingbat.warmup1;
 /**
- * We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+ * We have a loud talking parrot.
+ * The "hour" parameter is the current hour time in the range 0..23.
+ * We are in trouble if the parrot is talking and the hour is before 7 or after 20.
+ * Return true if we are in trouble.
  *
  *
  * parrotTrouble(true, 6) → true
