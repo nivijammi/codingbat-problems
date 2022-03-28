06b29c635f2d316966b329de2b642428f5af98ed
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-27 23:50:25 -0400

    intial test commit

--- src/test/java/edu/codingbat/warmup1/NearHundredTest.java
+++ src/test/java/edu/codingbat/warmup1/NearHundredTest.java
@@ -1,4 +1,23 @@
+package edu.codingbat.warmup1;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class NearHundredTest {
-  
+    @Test
+    public void shouldReturnTrueIf10or100or200() {
+        NearHundred object = new NearHundred();
+        assertFalse("not near 200", object.nearHundred(290));
+    }
+    @Test
+    public void shouldReturnTrueIf10or100or2001() {
+        NearHundred object = new NearHundred();
+        assertFalse("not near 0", object.nearHundred(-290));
+    }
+    @Test
+    public void shouldReturnTrueIf10or100or2002() {
+        NearHundred object = new NearHundred();
+        assertTrue("not near 200", object.nearHundred(191));
+    }
 }
