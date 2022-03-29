8dba85d35253d2818268e7b2c709c6580d2e0172
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-28 22:30:30 -0400

    Initial commit

--- src/main/java/edu/codingbat/warmup1/FrontBack.java
+++ src/main/java/edu/codingbat/warmup1/FrontBack.java
@@ -1,2 +1,28 @@
-package edu.codingbat.warmup1;public class FrontBack {
+package edu.codingbat.warmup1;
+/**
+ *
+ Given a string, return a new string where the first and last chars have been exchanged.
+
+
+ frontBack("code") → "eodc"
+ frontBack("a") → "a"
+ frontBack("ab") → "ba"
+
+
+ */
+
+public class FrontBack {
+    public String frontBack(String str) {
+        if (str.length()<= 1) {
+            System.out.println(str);
+            return str;
+        }
+        String a = str.substring(1,str.length()-1);
+        String newString = str.charAt(str.length()-1) + a + str.charAt(0);
+        System.out.println(newString);
+        return newString;
+
+    }
+
+
 }
