d308769861ecaca44f4e2bf4dbe6b8949bd228d1
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-28 22:29:48 -0400

    Initial commit

--- src/main/java/edu/codingbat/warmup1/front3.java
+++ src/main/java/edu/codingbat/warmup1/front3.java
@@ -1,2 +1,26 @@
-package edu.codingbat.warmup1;public class front3 {
+package edu.codingbat.warmup1;
+/**
+ * Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+ *
+ * front3("Java") → "JavJavJav"
+ * front3("Chocolate") → "ChoChoCho"
+ * front3("abc") → "abcabcabc"
+
+ */
+
+public class front3 {
+    public String front3(String str) {
+
+        if (str.length()>=3){
+            String newString = str.substring(0,3)+str.substring(0,3)+str.substring(0,3);
+            System.out.println(newString);
+            return newString;
+
+        }
+        else {
+            String threeTimesStr = str+str+str;
+            System.out.println(threeTimesStr);
+            return threeTimesStr;
+        }
+    }
 }
