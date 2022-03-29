26232595cbc856ac70125c46fecec3737f027077
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-28 22:28:53 -0400

    Initial commit

--- src/test/java/edu/codingbat/warmup1/FrontBackTest.java
+++ src/test/java/edu/codingbat/warmup1/FrontBackTest.java
@@ -1,4 +1,32 @@
+package edu.codingbat.warmup1;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class FrontBackTest {
-  
+    @Test
+    public void shouldReturnNewStringWith1stAndLastExchanged1() {
+        FrontBack object = new FrontBack();
+        String expected = "ehocolatC";
+        String actual = object.frontBack("Chocolate");
+        assertEquals(expected,actual);
+
+    }
+    @Test
+    public void shouldReturnNewStringWith1stAndLastExchanged2() {
+        FrontBack object = new FrontBack();
+        String expected = "oellh";
+        String actual = object.frontBack("hello");
+        assertEquals(expected,actual);
+
+    }
+    @Test
+    public void shouldReturnNewStringWith1stAndLastExchanged3() {
+        FrontBack object = new FrontBack();
+        String expected = "a";
+        String actual = object.frontBack("a");
+        assertEquals(expected,actual);
+
+    }
 }
