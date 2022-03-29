a00a9247e3cf52437e696cf48a1ef4cfd8041e25
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-28 22:50:05 -0400

    Initial commit

--- src/test/java/edu/codingbat/warmup1/Or35Test.java
+++ src/test/java/edu/codingbat/warmup1/Or35Test.java
@@ -1,4 +1,32 @@
+package edu.codingbat.warmup1;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class Or35Test {
-  
+    @Test
+    public void IsMultipleOf3or51() {
+        Or35 Object = new Or35();
+
+        assertTrue(Object.or35(3));
+    }
+    @Test
+    public void IsMultipleOf3or52() {
+        Or35 Object = new Or35();
+
+        assertFalse(Object.or35(121));
+    }
+    @Test
+    public void IsMultipleOf3or53() {
+        Or35 Object = new Or35();
+
+        assertTrue(Object.or35(100));
+    }
+    @Test
+    public void IsMultipleOf3or54() {
+        Or35 Object = new Or35();
+
+        assertFalse(Object.or35(29));
+    }
 }
