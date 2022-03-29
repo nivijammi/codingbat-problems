6a56d362ad6cd1ed0c5824cd12ac596c9115dc5b
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-28 22:38:49 -0400

    Initial commit

--- src/test/java/edu/codingbat/warmup1/BackAroundTest.java
+++ src/test/java/edu/codingbat/warmup1/BackAroundTest.java
@@ -1,4 +1,41 @@
+package edu.codingbat.warmup1;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class BackAroundTest {
-  
+    @Test
+    public void shouldAddLastCharToFrontAndBack1() {
+        BackAround object = new BackAround();
+        String expected = "obooo";
+        String actual = object.backAround("boo");
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    public void shouldAddLastCharToFrontAndBack2() {
+        BackAround object = new BackAround();
+        String expected = "cabcc";
+        String actual = object.backAround("abc");
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    public void shouldAddLastCharToFrontAndBack3() {
+        BackAround object = new BackAround();
+        String expected = "aaa";
+        String actual = object.backAround("a");
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    public void shouldAddLastCharToFrontAndBack4() {
+        BackAround object = new BackAround();
+        String expected = "dreadd";
+        String actual = object.backAround("read");
+        assertEquals(expected, actual);
+
+    }
+
 }
