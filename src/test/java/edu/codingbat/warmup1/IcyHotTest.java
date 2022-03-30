5025b8b4b4ac1523e9e32857f3ac1d2051c0f9e3
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-30 17:17:42 -0400

    Initial commit

--- src/test/java/edu/codingbat/warmup1/IcyHotTest.java
+++ src/test/java/edu/codingbat/warmup1/IcyHotTest.java
@@ -1,4 +1,23 @@
+package edu.codingbat.warmup1;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class IcyHotTest {
-  
+    @Test
+    public void shouldReturnTrueIfOnelessthan0AndOneGreaterThan100() {
+        IcyHot object = new IcyHot();
+        assertTrue(object.icyHot(-1,120));
+    }
+    @Test
+    public void shouldReturnTrueIfOnelessthan0AndOneGreaterThan1001() {
+        IcyHot object = new IcyHot();
+        assertFalse(object.icyHot(-2,-2));
+    }
+    @Test
+    public void shouldReturnTrueIfOnelessthan0AndOneGreaterThan1002() {
+        IcyHot object = new IcyHot();
+        assertFalse(object.icyHot(120,120));
+    }
 }
