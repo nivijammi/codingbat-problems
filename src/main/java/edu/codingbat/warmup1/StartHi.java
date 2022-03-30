3686db244c6bddce536e94e60f496d79008d5bf6
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-30 17:18:30 -0400

    Initial commit

--- src/main/java/edu/codingbat/warmup1/StartHi.java
+++ src/main/java/edu/codingbat/warmup1/StartHi.java
@@ -1,2 +1,28 @@
-package edu.codingbat.warmup1;public class StartHi {
+package edu.codingbat.warmup1;
+/**
+ * Given a string, return true if the string starts with "hi" and false otherwise.
+ *
+ *
+ * startHi("hi there") → true
+ * startHi("hi") → true
+ * startHi("hello hi") → false
+
+ */
+
+public class StartHi {
+    public boolean startHi(String str) {
+        if (str.length()<2){
+            System.out.println("false");
+            return false;
+        }
+
+        if (str.startsWith("hi")){
+            System.out.println("true");
+            return true;
+        }
+        else{
+            System.out.println("false");
+            return false;
+        }
+    }
 }
