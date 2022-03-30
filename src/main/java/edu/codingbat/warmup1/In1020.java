17acf14695570b4847f0eefe7abcf810977d8f00
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-30 19:37:43 -0400

    Initial commit

--- src/main/java/edu/codingbat/warmup1/In1020.java
+++ src/main/java/edu/codingbat/warmup1/In1020.java
@@ -1,2 +1,16 @@
-package edu.codingbat.warmup1;public class In1020 {
+package edu.codingbat.warmup1;
+
+public class In1020 {
+    public boolean in1020(int a, int b) {
+        if ((a>=10 && a<=20) || (b>=10 && b<=20)){
+            System.out.println("true");
+            return true;
+
+        }
+        else{
+            System.out.println("false");
+            return false;
+        }
+    }
+
 }
