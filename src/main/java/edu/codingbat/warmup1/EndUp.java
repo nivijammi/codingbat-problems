a539638577224bc6534c2eb522b744c760cd391d
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-31 18:40:19 -0400

    Initial commit

--- src/main/java/edu/codingbat/warmup1/EndUp.java
+++ src/main/java/edu/codingbat/warmup1/EndUp.java
@@ -1,2 +1,28 @@
-package edu.codingbat.warmup1;public class EndUp {
+package edu.codingbat.warmup1;
+/**
+ * Given a string, return a new string where the last 3 chars are now in upper case. If the string has less than 3 chars, uppercase whatever is there. Note that str.toUpperCase() returns the uppercase version of a string.
+ *
+ *
+ * endUp("Hello") → "HeLLO"
+ * endUp("hi there") → "hi thERE"
+ * endUp("hi") → "HI"
+ */
+
+public class EndUp {
+    public String endUp(String str) {
+        int length = str.length();
+        if(length < 3){
+            String upDatedStr = str.toUpperCase();
+            System.out.println(upDatedStr);
+            return upDatedStr;
+
+        }
+        int lastThree = str.length() -3;
+        String front = str.substring(0,lastThree);
+        String back = str.substring(lastThree);
+        String newString =front+back.toUpperCase();
+        System.out.println(newString);
+        return newString;
+    }
+
 }
