e4a88ee7686a2c08aebd027bc473163c4377830f
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-31 18:37:49 -0400

    Initial commit

--- src/main/java/edu/codingbat/warmup1/IntMax.java
+++ src/main/java/edu/codingbat/warmup1/IntMax.java
@@ -1,2 +1,30 @@
-package edu.codingbat.warmup1;public class IntMax {
+package edu.codingbat.warmup1;
+
+/**
+ * Given three int values, a b c, return the largest.
+ *
+ *
+ * intMax(1, 2, 3) → 3
+ * intMax(1, 3, 2) → 3
+ * intMax(3, 2, 1) → 3
+ */
+
+public class IntMax {
+    public int intMax(int a, int b, int c) {
+        if(a>b && a>c){
+            System.out.println(a);
+            return a;
+        }
+        if (b>a && b>c){
+            System.out.println(b);
+            return b;
+
+        }
+        else{
+            System.out.println(c);
+            return c;
+        }
+
+    }
+
 }
