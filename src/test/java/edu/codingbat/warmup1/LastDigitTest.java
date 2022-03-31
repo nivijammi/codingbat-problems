5fa70e4659f03d77fbcc588635e4a9796cbc5918
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-31 18:30:38 -0400

    Initial commit

--- src/test/java/edu/codingbat/warmup1/LastDigitTest.java
+++ src/test/java/edu/codingbat/warmup1/LastDigitTest.java
@@ -1,4 +1,28 @@
+package edu.codingbat.warmup1;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class LastDigitTest {
-  
+    @Test
+    public void shouldReturnTrueIfLastDigitInTwoGivenIntAreSam1() {
+        LastDigit object = new LastDigit();
+        assertTrue(object.lastDigit(10, 0));
+    }
+    @Test
+    public void shouldReturnTrueIfLastDigitInTwoGivenIntAreSame2() {
+        LastDigit object = new LastDigit();
+        assertTrue(object.lastDigit(114, 4));
+    }
+    @Test
+    public void shouldReturnTrueIfLastDigitInTwoGivenIntAreSame3() {
+        LastDigit object = new LastDigit();
+        assertFalse(object.lastDigit(11, 0));
+    }
+    @Test
+    public void shouldReturnTrueIfLastDigitInTwoGivenIntAreSame4() {
+        LastDigit object = new LastDigit();
+        assertFalse(object.lastDigit(11, 0));
+    }
 }
