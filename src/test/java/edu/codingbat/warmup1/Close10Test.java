6e8e2040ab0e7bf258edc35487bbc9b10bc09278
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-31 18:35:22 -0400

    Initial commit

--- src/test/java/edu/codingbat/warmup1/Close10Test.java
+++ src/test/java/edu/codingbat/warmup1/Close10Test.java
@@ -1,4 +1,37 @@
+package edu.codingbat.warmup1;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class Close10Test {
-  
+    @Test
+    public void shouldReturnWhicheverValueIsNearestTo10Or01() {
+        Close10 object = new Close10();
+        int expected =0;
+        int actual = object.close10(10, 10);
+        assertEquals(expected,actual);
+
+    }
+    @Test
+    public void shouldReturnWhicheverValueIsNearestTo102() {
+        Close10 object = new Close10();
+        int expected =9;
+        int actual= object.close10(9, 13);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldReturnWhicheverValueIsNearestTo103() {
+        Close10 object = new Close10();
+        int expected =0;
+        int actual= object.close10(13, 7);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldReturnWhicheverValueIsNearestTo104() {
+        Close10 object = new Close10();
+        int expected = 5;
+        int actual= object.close10(5, 21);
+        assertEquals(expected,actual);
+    }
 }
