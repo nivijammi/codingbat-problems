d9d013585d9381aa7746ea1764fc11140f8de4dd
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-31 19:28:19 -0400

    Initial commit

--- src/main/java/edu/codingbat/warmup2/StringTimes.java
+++ src/main/java/edu/codingbat/warmup2/StringTimes.java
@@ -1,2 +1,21 @@
-package edu.codingbat.warmup2;public class StringTimes {
+package edu.codingbat.warmup2;
+
+/**
+ * Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+ *
+ *
+ * stringTimes("Hi", 2) → "HiHi"
+ * stringTimes("Hi", 3) → "HiHiHi"
+ * stringTimes("Hi", 1) → "Hi"
+ */
+
+public class StringTimes {
+    public String stringTimes(String str, int n) {
+        String largerStr = "";
+        for (int counter = 0; counter < n; counter++){
+            largerStr = largerStr + str;
+        }
+        System.out.println(largerStr);
+        return largerStr;
+    }
 }
