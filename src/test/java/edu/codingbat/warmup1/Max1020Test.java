2e2cb183c298abf7822013a29d9356c77c2b9da3
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-31 18:28:10 -0400

    Initial commit

--- src/test/java/edu/codingbat/warmup1/Max1020Test.java
+++ src/test/java/edu/codingbat/warmup1/Max1020Test.java
@@ -1,4 +1,38 @@
+package edu.codingbat.warmup1;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class Max1020Test {
-  
+    @Test
+    public void shouldReturnTheLargerValueInRangeOf10To20InclusiveOrReturn01() {
+        Max1020 object = new Max1020();
+        int expected = 17;
+        int actual = object.max1020(17, 16);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldReturnTheLargerValueInRangeOf10To20InclusiveOrReturn02() {
+        Max1020 object = new Max1020();
+        int expected = 20;
+        int actual = object.max1020(20, 10);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldReturnTheLargerValueInRangeOf10To20InclusiveOrReturn03(){
+        Max1020 object = new Max1020();
+        int expected = 10;
+        int actual = object.max1020(21, 10);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldReturnTheLargerValueInRangeOf10To20InclusiveOrReturn04() {
+        Max1020 object = new Max1020();
+        int expected = 19;
+        int actual = object.max1020(19, 11);
+        assertEquals(expected,actual);
+    }
+
+
 }
