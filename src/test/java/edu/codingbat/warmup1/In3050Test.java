8a87b75d8a6bc4b05c4b437c9d87e46c1efdb5ae
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-31 18:32:47 -0400

    Initial commit

--- src/test/java/edu/codingbat/warmup1/In3050Test.java
+++ src/test/java/edu/codingbat/warmup1/In3050Test.java
@@ -1,4 +1,32 @@
+package edu.codingbat.warmup1;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class In3050Test {
-  
+    @Test
+    public void shouldReturnTrueIfBothGivenValInTheRange30tO40InclusiveOr40To50Inclusive1() {
+        In3050 object = new In3050();
+        assertFalse(object.in3050(35, 45));
+
+    }
+    @Test
+    public void shouldReturnTrueIfBothGivenValInTheRange30tO40InclusiveOr40To50Inclusive2() {
+        In3050 object = new In3050();
+        assertFalse(object.in3050(50, 51));
+
+    }
+    @Test
+    public void shouldReturnTrueIfBothGivenValInTheRange30tO40InclusiveOr40To50Inclusive3() {
+        In3050 object = new In3050();
+        assertTrue(object.in3050(40, 39));
+
+    } @Test
+    public void shouldReturnTrueIfBothGivenValInTheRange30tO40InclusiveOr40To50Inclusive4() {
+        In3050 object = new In3050();
+        assertTrue(object.in3050(40, 50));
+
+    }
+
 }
