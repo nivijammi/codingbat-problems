1643d900239e3c00154e6fcf919b25fd4ce7bd87
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-31 18:35:57 -0400

    Initial commit

--- src/main/java/edu/codingbat/warmup1/StringE.java
+++ src/main/java/edu/codingbat/warmup1/StringE.java
@@ -1,2 +1,30 @@
-package edu.codingbat.warmup1;public class StringE {
+package edu.codingbat.warmup1;
+
+/**
+ * Return true if the given string contains between 1 and 3 'e' chars.
+ *
+ *
+ * stringE("Hello") → true
+ * stringE("Heelle") → true
+ * stringE("Heelele") → false
+ */
+
+public class StringE {
+    public boolean stringE(String str) {
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'e') {
+                count++;
+            }
+
+        }
+        if (count >= 1 && count <= 3) {
+            System.out.println("True");
+            return true;
+        } else {
+            System.out.println("False");
+            return false;
+        }
+    }
 }
