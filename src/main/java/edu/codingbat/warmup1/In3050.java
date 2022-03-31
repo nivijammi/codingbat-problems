5dd565a5443fc53d24788db169eb19dbbfabc70a
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-31 18:38:50 -0400

    Initial commit

--- src/main/java/edu/codingbat/warmup1/In3050.java
+++ src/main/java/edu/codingbat/warmup1/In3050.java
@@ -1,2 +1,28 @@
-package edu.codingbat.warmup1;public class In3050 {
+package edu.codingbat.warmup1;
+
+/**
+ * Given 2 int values, return true if they are both in the range 30..40 inclusive, or they are both in the range 40..50 inclusive.
+ *
+ *
+ * in3050(30, 31) → true
+ * in3050(30, 41) → false
+ * in3050(40, 50) → true
+ */
+
+public class In3050 {
+    public boolean in3050(int a, int b) {
+        if((a>=30 && a<=40) && (b>=30 && b<=40)){
+            System.out.println("true");
+            return true;
+
+        }
+        if ((a>=40 && a<=50) && (b>=40 && b<=50)){
+            System.out.println("true");
+            return true;
+        }
+        else{
+            System.out.println("false");
+            return false;
+        }
+    }
 }
