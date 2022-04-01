b7444d699cebbd55203b69dd9234bdff750dfb7a
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-01 14:45:00 -0400

    Initial commit

--- src/main/java/edu/codingbat/warmup2/CountXX.java
+++ src/main/java/edu/codingbat/warmup2/CountXX.java
@@ -1,2 +1,26 @@
-package edu.codingbat.warmup2;public class CountXX {
+package edu.codingbat.warmup2;
+
+/**
+ * Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+ *
+ *
+ * countXX("abcxx") → 1
+ * countXX("xxx") → 2
+ * countXX("xxxx") → 3
+ */
+
+public class CountXX {
+    public int countXX(String str) {
+        int strLength = str.length();
+        int countOfX = 0;
+
+        for (int i = 1; i <strLength; i++) {
+            if (str.charAt(i) == 'x' && str.charAt(i - 1) == 'x') {
+                countOfX++;
+            }
+        }
+        System.out.println(countOfX);
+        return countOfX;
+    }
+
 }
