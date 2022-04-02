4a4a6c5755fc0e17263f6df2a6e343f1d7bb443e
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-01 23:48:55 -0400

    Initial commit

--- src/main/java/edu/codingbat/warmup2/Last2.java
+++ src/main/java/edu/codingbat/warmup2/Last2.java
@@ -1,29 +1,38 @@
 package edu.codingbat.warmup2;
 
+/**
+ * Given a string, return the count of the number of times that a substring
+ * length 2 appears in the string and also as the last 2 chars of the string,
+ * so "hixxxhi" yields 1 (we won't count the end substring).
+ *
+ * last2("hixxhi") → 1
+ * last2("xaxxaxaxx") → 1
+ * last2("axxxaaxx") → 2
+ */
+
 public class Last2 {
     public int last2(String str) {
-        int temp =0;
-        for(int i=0;i<str.length()-2;i++) {
-            String sub = str.substring(i, i+2);
-            String end = str.substring(str.length()-2);
-            if (sub.equals(end)) {
-                temp = temp+1;
-                System.out.println(temp);
+        String subStrI = "";
+        String endSubStr = "";
+        int count = 0;
+
+        if (str.length()> 2) {
+            endSubStr = str.substring(str.length()-2);
+            for (int i = 0; i < str.length() - 2; i++) {
+                subStrI = str.substring(i,i+2);
+                System.out.println("subStrI: " + " " + subStrI);
+                if(subStrI.equals(endSubStr)){
+                    count++;
+                }
 
             }
-
         }
-        return temp;
-
-
+        System.out.println(count);
+        return count;
     }
-    public static void main(String [] args){
+
+    public static void main(String[] args) {
         Last2 object = new Last2();
         object.last2("hixxhi");
-        object.last2("xaxxaxaxx");
-
     }
-
-
-
 }
