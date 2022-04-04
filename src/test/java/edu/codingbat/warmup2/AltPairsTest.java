89c0407f0d7a094edbb56c5b509cdefc4a070409
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-04 01:02:23 -0400

    Initial commit

--- src/test/java/edu/codingbat/warmup2/AltPairsTest.java
+++ src/test/java/edu/codingbat/warmup2/AltPairsTest.java
@@ -1,4 +1,36 @@
+package edu.codingbat.warmup2;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class AltPairsTest {
-  
+    @Test
+    public void shouldReturnAStringOfCharsAtIndexes014589One() {
+        AltPairs object = new AltPairs();
+        String expected ="ThThThth";
+        String actual = object.altPairs("ThisThatTheOther");
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldReturnAStringOfCharsAtIndexes014589Two() {
+        AltPairs object = new AltPairs();
+        String expected ="y";
+        String actual = object.altPairs("y");
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldReturnAStringOfCharsAtIndexes014589Three() {
+        AltPairs object = new AltPairs();
+        String expected ="ya";
+        String actual = object.altPairs("yak");
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldReturnAStringOfCharsAtIndexes014589Four() {
+        AltPairs object = new AltPairs();
+        String expected ="Chole";
+        String actual = object.altPairs("Chocolate");
+        assertEquals(expected,actual);
+    }
 }
