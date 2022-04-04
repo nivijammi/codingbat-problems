5f9b101a78c59934efe0790d9f6583a7d2173dd8
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-03 22:25:11 -0400

    Initial commit

--- src/main/java/edu/codingbat/warmup2/StringMatch.java
+++ src/main/java/edu/codingbat/warmup2/StringMatch.java
@@ -1,30 +1,28 @@
 package edu.codingbat.warmup2;
 
+/**
+ * Given 2 strings, a and b, return the number of the positions where
+ * they contain the same length 2 substring.
+ * So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az"
+ * substrings appear in the same place in both strings.
+ *
+ * stringMatch("xxcaazz", "xxbaaz") → 3
+ * stringMatch("abc", "abc") → 2
+ * stringMatch("abc", "axc") → 0
+ */
+
 public class StringMatch {
     public int stringMatch(String a, String b) {
 
-        int pos =0;
-        int len = Math.min(a.length(), b.length());
-        for(int i =0; i<len-1;i++) {
-            String subA = a.substring(i,i+2);
-            String subB = b.substring(i,i+2);
-            if (subA.equals(subB)){
-                pos= pos+1;
-                System.out.println("subA  "+ subA + "  " +"subB  " + subB);
+        int position = 0;
+        int length = Math.min(a.length(), b.length());
+
+        for (int i = 0; i < length - 1; i++) {
+            if (a.substring(i, i + 2).equals(b.substring(i, i + 2))){
+                position++;
             }
         }
-        System.out.println(pos );
-        return pos;
+        System.out.println(position);
+        return position;
     }
-
-
-    public static void main(String args[]) {
-        StringMatch object = new StringMatch();
-        object.stringMatch("abc", "abc");
-        object.stringMatch("", "hello");
-
-
-
-    }
-
 }
