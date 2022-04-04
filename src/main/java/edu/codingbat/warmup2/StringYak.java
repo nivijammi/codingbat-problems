a8629e2cd3f582f6ace90ccb4bf7a9bf5cb30a25
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-04 01:01:52 -0400

    Initial commit

--- src/main/java/edu/codingbat/warmup2/StringYak.java
+++ src/main/java/edu/codingbat/warmup2/StringYak.java
@@ -1,2 +1,32 @@
-package edu.codingbat.warmup2;public class StringYak {
+package edu.codingbat.warmup2;
+
+/**
+ * Suppose the string "yak" is unlucky.
+ * Given a string, return a version where all the "yak" are removed,
+ * but the "a" can be any char. The "yak" strings will not overlap.
+ *
+ *
+ * stringYak("yakpak") → "pak"
+ * stringYak("pakyak") → "pak"
+ * stringYak("yak123ya") → "123ya"
+ */
+
+public class StringYak {
+    public String stringYak(String str) {
+        String newString = "";
+
+
+        for (int i=0; i<str.length(); i++) {
+            // Look for i starting a "yak" -- advance i in that case
+
+            if (i+2<str.length() && str.charAt(i)=='y' && str.charAt(i+2)=='k') {
+                i =  i + 2;
+            } else { // Otherwise do the normal append
+                newString = newString + str.charAt(i);
+            }
+        }
+        System.out.println(newString);
+        return newString;
+    }
+
 }
