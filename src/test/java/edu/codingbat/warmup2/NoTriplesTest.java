847b34f4dd2b9c5eec1fff7ec60d781e9714ceb8
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-04 01:44:01 -0400

    Initial commit

--- src/test/java/edu/codingbat/warmup2/NoTriplesTest.java
+++ src/test/java/edu/codingbat/warmup2/NoTriplesTest.java
@@ -1,4 +1,32 @@
+package edu.codingbat.warmup2;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class NoTriplesTest {
-  
+    @Test
+    public void shouldReturnFalseIfTripleValAppearsThreeTimesInARow1() {
+        NoTriples object = new NoTriples();
+        int nums[] = {1};
+        assertTrue(object.noTriples(nums));
+    }
+    @Test
+    public void shouldReturnFalseIfTripleValAppearsThreeTimesInARow2() {
+        NoTriples object = new NoTriples();
+        int nums[] = {1,1,1};
+        assertFalse(object.noTriples(nums));
+    }
+    @Test
+    public void shouldReturnFalseIfTripleValAppearsThreeTimesInARow3() {
+        NoTriples object = new NoTriples();
+        int nums[] = {1, 1, 2, 2, 1, 2, 1};
+        assertTrue(object.noTriples(nums));
+    }
+    @Test
+    public void shouldReturnFalseIfTripleValAppearsThreeTimesInARow4() {
+        NoTriples object = new NoTriples();
+        int nums[] = {1, 1, 2, 2, 2, 1};
+        assertFalse(object.noTriples(nums));
+    }
 }
