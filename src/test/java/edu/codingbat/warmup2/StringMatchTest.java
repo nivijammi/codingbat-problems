807c1153f046692f8bb61e8385a6161b8b8b7055
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-03 22:23:43 -0400

    Initial commit

--- src/test/java/edu/codingbat/warmup2/StringMatchTest.java
+++ src/test/java/edu/codingbat/warmup2/StringMatchTest.java
@@ -1,4 +1,36 @@
+package edu.codingbat.warmup2;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class StringMatchTest {
-  
+    @Test
+    public void shouldReturnNoOfPositionsWhereStringAandBContainSameLength2Substring0() {
+        StringMatch object = new StringMatch();
+        int expected = 3;
+        int actual = object.stringMatch("xxcaazz", "xxbaaz");
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldReturnNoOfPositionsWhereStringAandBContainSameLength2Substring1() {
+        StringMatch object = new StringMatch();
+        int expected = 1;
+        int actual = object.stringMatch("aabbccdd", "abbbxxd");
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldReturnNoOfPositionsWhereStringAandBContainSameLength2Substring2() {
+        StringMatch object = new StringMatch();
+        int expected = 0;
+        int actual = object.stringMatch("", "hello");
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldReturnNoOfPositionsWhereStringAandBContainSameLength2Substring3() {
+        StringMatch object = new StringMatch();
+        int expected = 1;
+        int actual = object.stringMatch("he", "hello");
+        assertEquals(expected,actual);
+    }
 }
