b88d02216e9aae83e7741ed18ea8b49e1106ffa1
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-04 01:42:52 -0400

    Initial commit

--- src/test/java/edu/codingbat/warmup2/Array667Test.java
+++ src/test/java/edu/codingbat/warmup2/Array667Test.java
@@ -1,4 +1,40 @@
+package edu.codingbat.warmup2;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class Array667Test {
-  
+    @Test
+    public void shouldReturnTheCountWhenTwo6AreNextToEachOtherAndAloWhereSecond6IsA7One() {
+        Array667 object = new Array667();
+        int[] nums = {1, 2, 3, 6, 6};
+        int expected =1;
+        int actual = object.array667(nums);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldReturnTheCountWhenTwo6AreNextToEachOtherAndAloWhereSecond6IsA7Two() {
+        Array667 object = new Array667();
+        int[] nums = {6, 7, 6, 6};
+        int expected =2;
+        int actual = object.array667(nums);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldReturnTheCountWhenTwo6AreNextToEachOtherAndAloWhereSecond6IsA7Three() {
+        Array667 object = new Array667();
+        int[] nums = {6,1};
+        int expected =0;
+        int actual = object.array667(nums);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldReturnTheCountWhenTwo6AreNextToEachOtherAndAloWhereSecond6IsA7Four() {
+        Array667 object = new Array667();
+        int[] nums = {};
+        int expected =0;
+        int actual = object.array667(nums);
+        assertEquals(expected,actual);
+    }
 }
