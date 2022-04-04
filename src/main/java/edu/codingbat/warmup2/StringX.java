dc050244ae0e52b5f06c86b53787245976ac3f2b
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-03 22:24:39 -0400

    Initial commit

--- src/main/java/edu/codingbat/warmup2/StringX.java
+++ src/main/java/edu/codingbat/warmup2/StringX.java
@@ -1,2 +1,37 @@
-package edu.codingbat.warmup2;public class StringX {
+package edu.codingbat.warmup2;
+
+/**
+ * Given a string, return a version where all the "x" have been removed.
+ * Except an "x" at the very start or end should not be removed.
+ *
+ *
+ * stringX("xxHxix") → "xHix"
+ * stringX("abxxxcd") → "abcd"
+ * stringX("xabxxxcdx") → "xabcdx"
+ */
+
+public class StringX {
+    public String stringX(String str) {
+        String newString = "";
+
+        if (str.length() < 2) {
+            return str;
+        }
+        //adds the first char to newString irrespective of 'x'
+        newString = newString + str.charAt(0);
+
+        // add char to newString that are not 'x'
+        for (int i = 1; i < str.length() - 1; i++) {
+            if (str.charAt(i) != 'x') {
+                newString = newString + String.valueOf(str.charAt(i));
+            }
+        }
+        //adds the last char to newString irrespective of 'x'
+        newString = newString + str.substring(str.length() - 1);
+
+        System.out.println(newString);
+        return newString;
+    }
+
+
 }
