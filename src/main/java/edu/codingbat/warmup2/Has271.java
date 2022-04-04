29f021725e503c78c2affb3a4bc8fee6d9aa6a2d
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-04 01:42:18 -0400

    Initial commit

--- src/main/java/edu/codingbat/warmup2/Has271.java
+++ src/main/java/edu/codingbat/warmup2/Has271.java
@@ -1,19 +1,27 @@
 package edu.codingbat.warmup2;
 
+/**
+ * Given an array of ints, return true if it contains a 2, 7, 1 pattern:
+ * a value, followed by the value plus 5, followed by the value minus 1.
+ * Additionally the 271 counts even if the "1" differs by 2 or less from the correct value.
+ *
+ *
+ * has271([1, 2, 7, 1]) → true
+ * has271([1, 2, 8, 1]) → false
+ * has271([2, 7, 1]) → true
+ */
+
 public class Has271 {
     public boolean has271(int[] nums) {
-        // Iterate < length-2, so can use i+1 and i+2 in the loop.
-        // Return true immediately when seeing 271.
-        for (int i=0; i < (nums.length-2); i++) {
-            int val = nums[i];
-            if (nums[i+1] == (val+5) &&              // the "7" check
-                    Math.abs(nums[i+2] - (val-1)) <= 2) {  // the "1" check
-                System.out.println("true");
-                return true;
+        if(nums.length>=2){
+            for(int i=0;i<nums.length-2;i++){
+                if(nums[i+1] == nums[i]+5 && Math.abs(nums[i+2] - (nums[i]-1)) <= 2){
+                    System.out.println("true");
+                    return true;
+
+                }
             }
         }
-
-        // If we get here ... none found.
         System.out.println("false");
         return false;
     }
