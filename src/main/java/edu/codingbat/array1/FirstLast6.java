7733fbfda776e2f762f728aa91da2aef6a4148d2
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-05 13:18:43 -0400

    Initial commit

--- src/main/java/edu/codingbat/array1/FirstLast6.java
+++ src/main/java/edu/codingbat/array1/FirstLast6.java
@@ -1,2 +1,24 @@
-package edu.codingbat.array1;public class FirstLast6 {
+package edu.codingbat.array1;
+
+/**
+ * Given an array of ints, return true if 6 appears
+ * as either the first or last element in the array.
+ * The array will be length 1 or more.
+ *
+ * firstLast6([1, 2, 6]) → true
+ * firstLast6([6, 1, 2, 3]) → true
+ * firstLast6([13, 6, 1, 2, 3]) → false
+ */
+
+public class FirstLast6 {
+    public boolean firstLast6(int[] nums){
+        for(int i=0; i<nums.length;i++){
+            if(nums[0]==6 || nums[nums.length-1]==6){
+                System.out.println("true");
+                return true;
+            }
+        }
+        System.out.println("false");
+        return false;
+    }
 }
