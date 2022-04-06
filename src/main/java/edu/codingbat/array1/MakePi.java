afab89e389a943d6454d1658302c5726e49b365e
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-06 11:09:09 -0400

    Initial commit

--- src/main/java/edu/codingbat/array1/MakePi.java
+++ src/main/java/edu/codingbat/array1/MakePi.java
@@ -1,2 +1,24 @@
-package edu.codingbat.array1;public class MakePi {
+package edu.codingbat.array1;
+
+import java.util.Arrays;
+
+/**
+ * Return an int array length 3 containing the first 3 digits
+ * of pi, {3, 1, 4}.
+
+ * makePi() → [3, 1, 4]
+ */
+
+public class MakePi {
+    public int[] makePi() {
+        int[] pi = {3,1,4};
+        System.out.println(Arrays.toString(pi));
+        return pi;
+
+    }
+
+    public static void main(String[] args) {
+        MakePi object = new MakePi();
+        object.makePi();
+    }
 }
