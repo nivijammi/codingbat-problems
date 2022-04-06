929877e04207c21d981523c2d8fb3d748376a914
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-06 11:10:35 -0400

    Initial commit

--- src/main/java/edu/codingbat/array1/Sum3.java
+++ src/main/java/edu/codingbat/array1/Sum3.java
@@ -1,2 +1,19 @@
-package edu.codingbat.array1;public class Sum3 {
+package edu.codingbat.array1;
+
+/**
+ * Given an array of ints length 3, return the sum of all the elements.
+ * sum3([1, 2, 3]) → 6
+ * sum3([5, 11, 2]) → 18
+ * sum3([7, 0, 0]) → 7
+ */
+
+public class Sum3 {
+    public int sum3(int [] nums){
+        int sum = 0;
+        for(int i=0;i<nums.length;i++){
+            sum = sum + nums[i];
+        }
+        System.out.println(sum);
+        return sum;
+    }
 }
