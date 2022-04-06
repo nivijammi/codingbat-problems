711166df0a9e57076b87468df75b4751f9d1a8a8
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-06 11:11:40 -0400

    Initial commit

--- src/test/java/edu/codingbat/array1/SameFirstLastTest.java
+++ src/test/java/edu/codingbat/array1/SameFirstLastTest.java
@@ -1,4 +1,38 @@
+package edu.codingbat.array1;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class SameFirstLastTest {
-  
+    @Test
+    public void shouldReturnTrueIfArrayLengthGreaterThan1AndFirstAndLastElementIsEqual1() {
+        SameFirstLast object = new SameFirstLast();
+        int[] nums = {1,2,3};
+        assertFalse(object.sameFirstLast(nums));
+
+    }
+    @Test
+    public void shouldReturnTrueIfArrayLengthGreaterThan1AndFirstAndLastElementIsEqual2() {
+        SameFirstLast object = new SameFirstLast();
+        int[] nums = {7};
+        assertTrue(object.sameFirstLast(nums));
+
+    }
+    @Test
+    public void shouldReturnTrueIfArrayLengthGreaterThan1AndFirstAndLastElementIsEqual3() {
+        SameFirstLast object = new SameFirstLast();
+        int[] nums = {};
+        assertFalse(object.sameFirstLast(nums));
+
+    }
+    @Test
+    public void shouldReturnTrueIfArrayLengthGreaterThan1AndFirstAndLastElementIsEqual4() {
+        SameFirstLast object = new SameFirstLast();
+        int[] nums = {7,7};
+        assertTrue(object.sameFirstLast(nums));
+
+    }
+
+
 }
