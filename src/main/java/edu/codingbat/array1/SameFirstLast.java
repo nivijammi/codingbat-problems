a8040aa0e8cee93c1e0117423487a874427647e2
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-06 11:09:39 -0400

    Initial commit

--- src/main/java/edu/codingbat/array1/SameFirstLast.java
+++ src/main/java/edu/codingbat/array1/SameFirstLast.java
@@ -1,4 +1,22 @@
 package edu.codingbat.array1;
 
+/**
+ * Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal.
+  *
+ * sameFirstLast([1, 2, 3]) → false
+ * sameFirstLast([1, 2, 3, 1]) → true
+ * sameFirstLast([1, 2, 1]) → true
+ */
+
 public class SameFirstLast {
+    public boolean sameFirstLast(int[] nums){
+        if(nums.length>=1){
+            for (int i =0; i<nums.length;i++){
+                if(nums[0]==nums[nums.length-1]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
