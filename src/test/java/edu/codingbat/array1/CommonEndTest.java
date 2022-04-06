b68612e76820174d0977e9a94b4cabafc01f2e05
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-06 11:11:08 -0400

    Initial commit

--- src/test/java/edu/codingbat/array1/CommonEndTest.java
+++ src/test/java/edu/codingbat/array1/CommonEndTest.java
@@ -1,4 +1,43 @@
+package edu.codingbat.array1;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class CommonEndTest {
-  
+    @Test
+    public void shouldReturnTrueIfTwoGivenArraysHaveSameFirstOrLastElements1() {
+        CommonEnd object = new CommonEnd();
+        int [] a = {1,2,3};
+        int [] b = {7,3};
+        assertTrue(object.commonEnd(a,b));
+    }
+    @Test
+    public void shouldReturnTrueIfTwoGivenArraysHaveSameFirstOrLastElements2() {
+        CommonEnd object = new CommonEnd();
+        int [] a = {1,2,3};
+        int [] b = {7,3,2};
+        assertFalse(object.commonEnd(a,b));
+    }
+    @Test
+    public void shouldReturnTrueIfTwoGivenArraysHaveSameFirstOrLastElements3() {
+        CommonEnd object = new CommonEnd();
+        int [] a = {1,2,3};
+        int [] b = {1,3};
+        assertTrue(object.commonEnd(a,b));
+    }
+    @Test
+    public void shouldReturnTrueIfTwoGivenArraysHaveSameFirstOrLastElements4() {
+        CommonEnd object = new CommonEnd();
+        int [] a = {1,2,3};
+        int [] b = {1};
+        assertTrue(object.commonEnd(a,b));
+    }
+    @Test
+    public void shouldReturnTrueIfTwoGivenArraysHaveSameFirstOrLastElements5() {
+        CommonEnd object = new CommonEnd();
+        int [] a = {1,2,3};
+        int [] b = {2};
+        assertFalse(object.commonEnd(a,b));
+    }
 }
