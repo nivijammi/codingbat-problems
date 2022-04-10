955ae70ee514956c38524eb9a7404c07e8f369f5
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-10 00:58:20 -0400

    Initial commit

--- src/test/java/edu/codingbat/array1/Sum2Test.java
+++ src/test/java/edu/codingbat/array1/Sum2Test.java
@@ -1,4 +1,38 @@
+package edu.codingbat.array1;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class Sum2Test {
-  
+    @Test
+    public void shouldReturnTheSumOfFirstTwoElements() {
+        Sum2 object = new Sum2();
+        int[] nums = {1,2,3};
+        int  expected =3;
+        int actual = object.sum2(nums);
+}
+    @Test
+    public void shouldReturnTheSumOfFirstTwoElements2() {
+        Sum2 object = new Sum2();
+        int[] nums = {1,1,1,1};
+        int  expected =2;
+        int actual = object.sum2(nums);
+    }
+    @Test
+    public void shouldReturnTheSumOfFirstTwoElements3() {
+        Sum2 object = new Sum2();
+        int[] nums = {};
+        int  expected =0;
+        int actual = object.sum2(nums);
+    }
+    @Test
+    public void shouldReturnTheSumOfFirstTwoElements4() {
+        Sum2 object = new Sum2();
+        int[] nums = {4};
+        int  expected =4;
+        int actual = object.sum2(nums);
+    }
+
+
 }
