5cda98e5a546edc3c869cbc8c54e9ff151a25f90
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-10 00:57:05 -0400

    Modified for test

--- src/main/java/edu/codingbat/array1/MaxEnd3.java
+++ src/main/java/edu/codingbat/array1/MaxEnd3.java
@@ -1,41 +1,34 @@
 package edu.codingbat.array1;
 
+import java.util.Arrays;
+
+/**
+ *
+ *         Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that        value. Return the changed array.
+ *
+ *
+ *     maxEnd3([1, 2, 3]) → [3, 3, 3]
+ *     maxEnd3([11, 5, 9]) → [11, 11, 11]
+ *     maxEnd3([2, 11, 3]) → [3, 3, 3]
+ *
+ *
+ */
+
 public class MaxEnd3 {
-    /*
-        Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that        value. Return the changed array.
 
+    public int[] maxEnd3(int[] nums) {
+        int max = 0;
+        if(nums[0]>nums[2]) {
+            max = nums[0];
 
-    maxEnd3([1, 2, 3]) → [3, 3, 3]
-    maxEnd3([11, 5, 9]) → [11, 11, 11]
-    maxEnd3([2, 11, 3]) → [3, 3, 3]
-
-     */
-    public int[] maxEnd3(int[] nums, int b) {
-        int n = nums.length;
-        if (nums[0]>nums[n-1]){
-            int[] newArray = {nums[0], nums[0], nums[0]};
-            return newArray;
+        }else{
+            max = nums[2];
         }
-        else{
-            int[] newArray = {nums[n-1],nums[n-1],nums[n-1]};
-            return newArray;
-        }
-
+        int newArray[] = new int[nums.length];
+        newArray[0]=max; newArray[1] = max; newArray[2]= max;
+        System.out.println(Arrays.toString(newArray));
+        return newArray;
     }
 
-    public boolean foo2(int[] nums, int b) {
-        int n = nums.length;
-        return nums[0] > nums[n - 1];
 
-    }
-
-    public static void main(String[]args){
-        MaxEnd3 object = new MaxEnd3();
-        int[] a = {1,2,3};//creating and intializing the array. however arrays are created by iterating through some list and creating the array.
-        int b = 100;
-        int[] c = object.maxEnd3(a, b);
-        boolean d = object.foo2(a, b);
-
-
-    }
 }
