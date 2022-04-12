96b3e5a22e450452f7994724a192b6dbef7ebaa1
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-11 22:37:25 -0400

    Initial commit

--- src/test/java/edu/codingbat/array1/MakeEndsTest.java
+++ src/test/java/edu/codingbat/array1/MakeEndsTest.java
@@ -1,4 +1,36 @@
+package edu.codingbat.array1;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class MakeEndsTest {
-  
+    @Test
+    public void  shouldReturnANewArrayContainingFirstAndLastElementsFromTheOriginalArray1() {
+        MakeEnds object = new MakeEnds();
+        int[] nums = {2,3,4,1};
+        int[] expected ={2,1};
+        int [] actual = object.makeEnds(nums);
+    }
+    @Test
+    public void  shouldReturnANewArrayContainingFirstAndLastElementsFromTheOriginalArray2() {
+        MakeEnds object = new MakeEnds();
+        int[] nums = {7};
+        int[] expected ={7,7};
+        int [] actual = object.makeEnds(nums);
+    }
+    @Test
+    public void  shouldReturnANewArrayContainingFirstAndLastElementsFromTheOriginalArray3() {
+        MakeEnds object = new MakeEnds();
+        int[] nums = {1, 2, 2, 2, 2, 2, 2, 3};
+        int[] expected ={1,3};
+        int [] actual = object.makeEnds(nums);
+    }
+    @Test
+    public void  shouldReturnANewArrayContainingFirstAndLastElementsFromTheOriginalArray4() {
+        MakeEnds object = new MakeEnds();
+        int[] nums = {1, 2, 3, 4};
+        int[] expected ={1,4};
+        int [] actual = object.makeEnds(nums);
+    }
 }
