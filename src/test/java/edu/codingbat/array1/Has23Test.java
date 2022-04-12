bbd64d142ddd771c6194ce0947843400fb27b784
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-11 23:19:50 -0400

    Initial commit

--- src/test/java/edu/codingbat/array1/Has23Test.java
+++ src/test/java/edu/codingbat/array1/Has23Test.java
@@ -1,4 +1,32 @@
+package edu.codingbat.array1;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class Has23Test {
-  
+    @Test
+    public void shouldReturnTrueIfItContainsA2OrA3One() {
+        Has23 object = new Has23();
+        int[] nums = {2,5};
+        assertTrue(object.has23(nums));
+    }
+    @Test
+    public void shouldReturnTrueIfItContainsA2OrA3Two() {
+        Has23 object = new Has23();
+        int[] nums = {4,5};
+        assertFalse(object.has23(nums));
+    }
+    @Test
+    public void shouldReturnTrueIfItContainsA2OrA3Three() {
+        Has23 object = new Has23();
+        int[] nums = {3,3};
+        assertTrue(object.has23(nums));
+    }
+    @Test
+    public void shouldReturnTrueIfItContainsA2OrA3Four() {
+        Has23 object = new Has23();
+        int[] nums = {9,5};
+        assertFalse(object.has23(nums));
+    }
 }
