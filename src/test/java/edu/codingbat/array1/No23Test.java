cb192dab9afa76d492321173f70096daaa8728af
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-18 23:11:27 -0400

    Initial commit

--- src/test/java/edu/codingbat/array1/No23Test.java
+++ src/test/java/edu/codingbat/array1/No23Test.java
@@ -1,4 +1,36 @@
+package edu.codingbat.array1;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class No23Test {
-  
+    @Test
+    public void shouldReturnTrueIfDoesNotContain2Or3One() {
+        No23 object = new No23();
+        int[] nums = {4,5};
+        assertTrue(object.no23(nums));
+    }
+    @Test
+    public void shouldReturnTrueIfDoesNotContain2Or3Two() {
+        No23 object = new No23();
+        int[] nums = {3,5};
+        assertFalse(object.no23(nums));
+    }
+    @Test
+    public void shouldReturnTrueIfDoesNotContain2Or3Three() {
+        No23 object = new No23();
+        int[] nums = {1,3};
+        assertFalse(object.no23(nums));
+    }
+    @Test
+    public void shouldReturnTrueIfDoesNotContain2Or3Four() {
+        No23 object = new No23();
+        int[] nums = {8,7};
+        assertTrue(object.no23(nums));
+    }
+
+
+
+
 }
