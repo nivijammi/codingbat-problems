a3cf0278d19d5ffc2bcf95d06a3bacc4d07e3997
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-19 16:39:14 -0400

    Initial commit

--- src/main/java/edu/codingbat/array1/MakeLast.java
+++ src/main/java/edu/codingbat/array1/MakeLast.java
@@ -1,2 +1,12 @@
-package edu.codingbat.array1;public class MakeLast {
+package edu.codingbat.array1;
+
+import java.util.Arrays;
+
+public class MakeLast {
+    public int[] makeLast(int[] nums) {
+        int[] newArray = new int [nums.length*2];
+        newArray[newArray.length-1] = nums[nums.length-1];
+        System.out.println(Arrays.toString(newArray));
+        return newArray;
+    }
 }
