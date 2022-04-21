33e5c5cacb55e2db059d3ed117a2d6ffdfb62fb3
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-20 23:04:37 -0400

    Initial commit

--- src/main/java/edu/codingbat/array1/Start1.java
+++ src/main/java/edu/codingbat/array1/Start1.java
@@ -1,2 +1,17 @@
-package edu.codingbat.array1;public class Start1 {
+package edu.codingbat.array1;
+
+/**
+ * Start with 2 int arrays, a and b, of any length.
+ * Return how many of the arrays have 1 as their first element.
+
+ * start1([1, 2, 3], [1, 3]) → 2
+ * start1([7, 2, 3], [1]) → 1
+ * start1([1, 2], []) → 1
+ */
+
+public class Start1 {
+    public int start1(int[] a, int[] b) {
+
+        return 0;
+    }
 }
